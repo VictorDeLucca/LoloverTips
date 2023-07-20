0c4c75bc54f884b8452556a258d0dc7a5df36325
Victor DeLucca <57101522+VictorDeLucca@users.noreply.github.com>
2023-07-20 02:29:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.use(express.static('public'));
 
